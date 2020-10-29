10581e7a3c202632a00b941199e69575ac808438
geetha <geetha.kanimozhi90@gmail.com>
2020-10-29 11:54:09 +0530

    first version of HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Geetha!");
+		
+		int myFirstNumber = 1;
+		int mySecondNumber =12;
+		int myThirdNumber =6;
+		
+		System.out.println(myFirstNumber);
+
+	}
+
+}
